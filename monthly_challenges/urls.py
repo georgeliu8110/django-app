65405d9f1b960b5dc179840124af34b0e50cb9b9
George Liu <georgeliu8110@gmail.com>
2024-03-05 23:12:46 -0600

    add dynamic urls

--- monthly_challenges/urls.py
+++ monthly_challenges/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('challenges/', include('challenges.urls')) # This line is added to include the challenges app's urls
 ]
